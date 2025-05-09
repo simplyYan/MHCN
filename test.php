067ec62e51c9e903114d64dca99d210b4999e903
Wesley Yan S. B. <102262759+simplyYan@users.noreply.github.com>
2025-05-09 21:39:05 +0000

    Create test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Info</title>
+</head>
+<body>
+    <?php phpinfo(); ?>
+<p>Hi MHCN!</p>
+</body>
+</html>
